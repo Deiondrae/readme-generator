c8dac2d082b03df9dbe350dbd240771ccbbfa6d7
Villager <deiondrae@gmail.com>
2022-06-23 14:06:20 -0400

    added questions array, inquirer prompts working, can write to readme

--- index.js
+++ index.js
@@ -4,8 +4,92 @@ const inquirer = require("inquirer");
 const generateReadme = require("./utils/generateMarkdown");
 
 // Array of questions for user input
-const questions = [];
-
+const questions = [
+    {
+      type: "input",
+      name: "title",
+      message: "What is the name of this project? (Required)",
+      validate: (title) => {
+        if (title) {
+          return true;
+        } else {
+          console.log("Please enter a title for the project!");
+          return false;
+        }
+      },
+    },
+    {
+      type: "input",
+      name: "description",
+      message: "Enter a description of this project (Required)",
+      validate: (description) => {
+        if (description) {
+          return true;
+        } else {
+          console.log("Please enter a description of the project!");
+          return false;
+        }
+      },
+    },
+    {
+      type: "input",
+      name: "author",
+      message: "Who is author of this project (Required)",
+      validate: (author) => {
+        if (author) {
+          return true;
+        } else {
+          console.log("Please enter the name of the users!");
+          return false;
+        }
+      },
+    },
+    {
+      type: "input",
+      name: "github",
+      message: "Enter your github username. (Required)",
+      validate: (github) => {
+        if (github) {
+          return true;
+        } else {
+          console.log("Please enter your github username!");
+          return false;
+        }
+      },
+    },
+    {
+      type: "input",
+      name: "installation",
+      message: "If your project requires installation, please walk through the installation process (Optional)",
+    },
+    {
+      type: "input",
+      name: "usage",
+      message: "Please provide instructions and examples for use. (Optional)",
+    },
+    {
+      type: "list",
+      name: "license",
+      message: "What license do you want to use for your project? (Optional)",
+      choices: [
+        "MIT",
+        "GPLv2",
+        "Apache",,
+        "BSD-3-Clause",
+        "None",
+      ],
+    },
+    {
+      type: "input",
+      name: "contribution",
+      message: "Please advise on how to contribute to this project. (Optional)",
+    },
+    {
+      type: "input",
+      name: "tests",
+      message: "Please provide examples of how to run any tests you've written (Optional)",
+    },
+];
 // write README file
 const writeToFile = data => {
     return new Promise((resolve, reject) => {
@@ -21,7 +105,7 @@ const writeToFile = data => {
           });
     });   
 };
-
+// Function to initialize app
 const init = (questions) => {
     return inquirer.prompt(questions).catch((error) => {
       if (error) {
